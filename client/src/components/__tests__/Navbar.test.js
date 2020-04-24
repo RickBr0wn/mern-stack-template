eb078cc53efe4b70c54db95c09617403c18a8bb1
Rick Brown <ricky.brown.00@gmail.com>
2020-04-24 17:39:47 +0100

    struggling with useAuthContext in Login.test.js

--- client/src/components/__tests__/Navbar.test.js
+++ client/src/components/__tests__/Navbar.test.js
@@ -7,28 +7,28 @@ import Navbar from '../Navbar'
 afterEach(cleanup)
 
 describe('<Navbar/>', () => {
-	it('should render a navbar', () => {
-		const { getByTestId } = renderAllTheProviders(<Navbar />)
-		expect(getByTestId('navbar')).toBeInTheDocument()
-	})
+  it('should render a navbar', () => {
+    const { getByTestId } = renderAllTheProviders(<Navbar />)
+    expect(getByTestId('navbar')).toBeInTheDocument()
+  })
 
-	it('should render 4 links', () => {
-		const { queryAllByTestId } = renderAllTheProviders(<Navbar />)
-		expect(queryAllByTestId('link')).toHaveLength(4)
-	})
+  it('should render 4 links', () => {
+    const { queryAllByTestId } = renderAllTheProviders(<Navbar />)
+    expect(queryAllByTestId('link')).toHaveLength(4)
+  })
 
-	it('should navigate to new page when a link is clicked', () => {
-		const { getByTestId, getByText } = renderAllTheProviders(
-			<App>
-				<Navbar />
-			</App>
-		)
-		expect(getByTestId('home')).toBeInTheDocument()
+  it('should navigate to new page when a link is clicked', () => {
+    const { getByTestId, getByText } = renderAllTheProviders(
+      <App>
+        <Navbar />
+      </App>
+    )
+    expect(getByTestId('home')).toBeInTheDocument()
 
-		fireEvent.click(getByText(/register/i))
-		expect(getByTestId('register')).toBeInTheDocument()
+    fireEvent.click(getByText(/register/i))
+    expect(getByTestId('register')).toBeInTheDocument()
 
-		fireEvent.click(getByText(/home/i))
-		expect(getByTestId('home')).toBeInTheDocument()
-	})
+    fireEvent.click(getByText(/home/i))
+    expect(getByTestId('home')).toBeInTheDocument()
+  })
 })
